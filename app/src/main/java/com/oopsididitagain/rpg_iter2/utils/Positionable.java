65335d7497dca790d0e41e1a083567aae59e040b
Joseph Liccini <joseph.liccini@ufl.edu>
2015-03-15 19:33:55 -0400

    Added Positionable Interface

---
+++ app/src/main/java/com/oopsididitagain/rpg_iter2/utils/Positionable.java
@@ -0,0 +1,8 @@
+package com.oopsididitagain.rpg_iter2.utils;
+
+import com.oopsididitagain.rpg_iter2.models.Position;
+
+public interface Positionable {
+	public Position getPosition();
+	public Position setPosition(Position position);
+}
