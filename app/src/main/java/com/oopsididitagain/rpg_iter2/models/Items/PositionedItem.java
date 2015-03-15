58cae85833b38c53786cb0d2fad7507ab4dd1c96
Joseph Liccini <joseph.liccini@ufl.edu>
2015-03-15 19:43:15 -0400

    Added PositionedItem, which implements the Positionable Interface. Removed Item.java.

---
+++ app/src/main/java/com/oopsididitagain/rpg_iter2/models/Items/PositionedItem.java
@@ -0,0 +1,25 @@
+package com.oopsididitagain.rpg_iter2.models.Items;
+
+import com.oopsididitagain.rpg_iter2.models.GameObject;
+import com.oopsididitagain.rpg_iter2.models.Position;
+import com.oopsididitagain.rpg_iter2.utils.Positionable;
+
+public class PositionedItem extends GameObject implements Positionable {
+	private Position position;
+
+	public PositionedItem(String id, Position position) {
+		super(id);
+		this.position = position;
+	}
+
+	@Override
+	public Position getPosition() {
+		return position;
+	}
+
+	@Override
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+}
--- app/src/main/java/com/oopsididitagain/rpg_iter2/utils/Positionable.java
+++ app/src/main/java/com/oopsididitagain/rpg_iter2/utils/Positionable.java
@@ -4,5 +4,5 @@ import com.oopsididitagain.rpg_iter2.models.Position;
 
 public interface Positionable {
 	public Position getPosition();
-	public Position setPosition(Position position);
+	public void setPosition(Position position);
 }
