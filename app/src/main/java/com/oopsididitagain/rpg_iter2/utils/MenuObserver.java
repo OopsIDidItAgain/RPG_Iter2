ece4bc570598da99ed48a0e9bd362b16e54583f7
tessbianchi <tesshbianchi@gmail.com>
2015-03-15 19:57:21 -0400

    Avatar Creation Controller

--- app/src/main/java/com/oopsididitagain/rpg_iter2/utils/MenuObserver.java
+++ app/src/main/java/com/oopsididitagain/rpg_iter2/utils/MenuObserver.java
@@ -1,4 +1,7 @@
 package com.oopsididitagain.rpg_iter2.utils;
+
+import java.awt.List;
+
 public interface MenuObserver {
-	public void update();
+	public void update(List<String> options,int size, int currentOption);
 }
