049d503f0a90d3c92e0ca33de72ff5f639e7b746
dannysepler <dsepler@ufl.edu>
2015-03-19 23:15:05 -0400

    map databases and other fun things!!

---
+++ app/src/main/java/com/oopsididitagain/rpg_iter2/assets/MapDatabase.java
@@ -0,0 +1,46 @@
+package com.oopsididitagain.rpg_iter2.assets;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MapDatabase {
+	BufferedReader gridReader;
+	File gridFile;
+	
+	BufferedReader objectsReader;
+	File objectsFile;
+	
+	public MapDatabase(String filepath) {
+		gridFile = new File("src/levels/"+filepath+"/grid.csv");
+		objectsFile = new File("src/levels/"+filepath+"/tileables.csv");
+		
+		try { 
+			gridReader = new BufferedReader(new FileReader(gridFile));
+			objectsReader = new BufferedReader(new FileReader(objectsFile));
+		} catch (FileNotFoundException e) {
+			System.out.println("Linking files to readers did not work....");
+			e.printStackTrace();
+		}
+	}
+	
+	public String readGridLine() {
+		return readNextLine(gridReader);
+	}
+	
+	public String readObjectsLine() {
+		return readNextLine(objectsReader);
+	}
+	
+	private String readNextLine(BufferedReader br) {
+		try {
+			return br.readLine();
+		} catch (IOException e) {
+			System.out.println("Reading line didn't work....");
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
--- app/src/main/java/com/oopsididitagain/rpg_iter2/models/GameMap.java
+++ app/src/main/java/com/oopsididitagain/rpg_iter2/models/GameMap.java
@@ -1,5 +1,6 @@
 package com.oopsididitagain.rpg_iter2.models;
 
+import com.oopsididitagain.rpg_iter2.assets.MapDatabase;
 import com.oopsididitagain.rpg_iter2.models.items.Item;
 import com.oopsididitagain.rpg_iter2.models.items.ObstacleItem;
 import com.oopsididitagain.rpg_iter2.models.Position;
@@ -11,10 +12,85 @@ import com.oopsididitagain.rpg_iter2.models.Tile;
 public class GameMap {
 	Tile[][] tiles;
 	// i don't think we need height or width attributes
+	MapDatabase mapReader;
 	
 	GameMap(Tile[][] tiles) {
-		
 		this.tiles = tiles;
+	}
+	
+	GameMap(String level) {
+		mapReader = new MapDatabase(level);
+	}
+	
+	// I believe it is better to have all the code for creating
+	// the game map inside of GameMap. It just seems right from an MVC
+	// perspective for the map to know how it is created -- thoughts? (Danny)
+	public void initialize() {
+		
+		// RIGHT, so we're reading from 2 csv's!
+		//
+		//		1. one that has positions and terrains
+		//		2. one that has positions and tileables (minus terrains)
+		
+		initializeGrid(); // this also initializes the map
+		initializeObjects();
+	}
+	
+	public void initializeGrid() {
+		// initialize map!!
+		String[] dimensions = mapReader.readGridLine().split(",");
+		tiles = new Tile[Integer.parseInt(dimensions[1])] 
+		                [Integer.parseInt(dimensions[0])];
+		
+		mapReader.readGridLine(); // blank line
+		
+		// go through the grid
+		String s = mapReader.readGridLine();
+		
+		for (int i = 0; i < Integer.parseInt(dimensions[1]); i++) {
+			s = mapReader.readGridLine();
+			
+			if (s.equals("")) {
+				// default behavior if something's wrong with the map
+				for (int j = 0; j < Integer.parseInt(dimensions[0]); j++)
+					tiles[i][j] = new Tile(new Position(i,j), Terrain.GRASS);
+				continue;
+			}
+			
+			String[] array = s.split(",");
+			
+			for ( int j = 0; j < array.length; j++ ) {
+				Position p = new Position(i,j);
+				switch(array[j]) {
+					case "^":
+						tiles[i][j] = new Tile( p, Terrain.MOUNTAIN );
+						break;
+					case " ":
+						tiles[i][j] = new Tile( p, Terrain.GRASS );
+						break;
+					case "~":
+						tiles[i][j] = new Tile( p, Terrain.WATER );
+						break;
+					default:
+						System.out.println("Error, unknown terrain type when reading map...");
+				}
+			}
+		}
+	}
+	
+	public void initializeObjects() {
+		String s;
+		while ((s = mapReader.readObjectsLine()).equals("")) {
+			// String[] array = s.split(",");
+			
+			// to do:
+			// decide how to figure out what kind of item it is
+			// 
+			// 		sample idea:
+			// int x = Integer.parseInt(array[1]);
+			// int y = Integer.parseInt(array[2]);
+			// tiles[y][x] = new Item( array[0] );
+		}
 	}
 	
 	public int getHeight() {
@@ -43,10 +119,10 @@ public class GameMap {
 	
 
 	public boolean checkIfValid(Position pos) {
-		if (	(pos.getY() >= getHeight() ||
+		if (	(pos.getY() >= getHeight()) ||
 				(pos.getX() >= getWidth()) ||
 				(pos.getY() < 0) ||
-				(pos.getX() < 0)))
+				(pos.getX() < 0))
 					return false;
 		
 		return true;
