01b2d00d0370418f2e1bcd36831be6ae3bbe05f5
tessbianchi <tesshbianchi@gmail.com>
2015-03-22 01:12:38 -0400

    fixed getting the skill of the thing

--- app/src/main/java/com/oopsididitagain/rpg_iter2/models/Game.java
+++ app/src/main/java/com/oopsididitagain/rpg_iter2/models/Game.java
@@ -3,8 +3,11 @@ package com.oopsididitagain.rpg_iter2.models;
 import com.oopsididitagain.rpg_iter2.assets.MapDatabase;
 import com.oopsididitagain.rpg_iter2.models.entities.Avatar;
 import com.oopsididitagain.rpg_iter2.models.entities.Npc;
+import com.oopsididitagain.rpg_iter2.models.items.TakeableItem;
+import com.oopsididitagain.rpg_iter2.models.items.WeaponTakeableItem;
 import com.oopsididitagain.rpg_iter2.models.stats.StatBlob;
 import com.oopsididitagain.rpg_iter2.utils.PositionOutOfBoundsException;
+import com.oopsididitagain.rpg_iter2.utils.WeaponItemType;
 
 public class Game {
 
@@ -15,10 +18,14 @@ public class Game {
 	public Game(Avatar avatar){
 		this.avatar = avatar;
 		gameMap = new GameMap(new MapDatabase(1));
-		gameMap.getTileAt((new Position(0,0))).add(avatar);
-		Position position = new Position(2,0);
+		Position position2 = new Position(3,0);
 		StatBlob statBlob = new StatBlob(0, 0, 0, 0, 0, 0, 0, 20, 20);
+		WeaponTakeableItem pgo = new WeaponTakeableItem("chainsaw_item", position2, 4.05,statBlob,WeaponItemType.ONE_HANDED_WEAPON);
+		gameMap.getTileAt((new Position(0,0))).add(avatar);
+		gameMap.getTileAt(position2).add(pgo);
+		Position position = new Position(2,0);
 		Npc buddy = new Npc("buddy", position, statBlob);
+		buddy.getInventory().add(pgo);
 		gameMap.getTileAt(position).add(buddy);
 	}
 	public Game( Avatar avatar, GameMap gameMap){
--- app/src/main/java/com/oopsididitagain/rpg_iter2/models/occupations/Occupation.java
+++ app/src/main/java/com/oopsididitagain/rpg_iter2/models/occupations/Occupation.java
@@ -1,10 +1,34 @@
 package com.oopsididitagain.rpg_iter2.models.occupations;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.oopsididitagain.rpg_iter2.models.Skill;
+import com.oopsididitagain.rpg_iter2.utils.Command;
 
 public abstract class Occupation {
-	public abstract void giveSkills(ArrayList<Skill> skillMap, ArrayList<Skill> fightSkillList, Map<String, Skill> passiveSkillList);
+	public abstract void giveSkills();
+	
+	protected ArrayList<Skill> gameSkillList = new ArrayList<Skill>();
+	protected ArrayList<Skill> fightSkillList = new ArrayList<Skill>();
+	protected Map<String,Skill> passiveSkillList = new HashMap<String,Skill>();
+	public ArrayList<Skill> getGameSkillList() {
+		return gameSkillList;
+	}
+	public ArrayList<Skill> getFightSkillList() {
+		return fightSkillList;
+	}
+	public Map<String, Skill> getPassiveSkillList() {
+		return passiveSkillList;
+	}
+	public Skill getPassiveSkill(String skill) {
+		return passiveSkillList.get(skill);
+	}
+	public abstract Skill getFightSkill(Command command);
+	public abstract Skill getActiveSkill(Command command);
+	
+
+	
+	
 }
